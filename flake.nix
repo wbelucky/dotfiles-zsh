e0e285e69ea55f89f41e1e8d889d4b76868ad170
wbelucky <39439193+WBelucky@users.noreply.github.com>
2025-03-30 19:59:18 +0900

    add flake.nix

---
+++ flake.nix
@@ -0,0 +1,32 @@
+{
+  description = "Home Manager configuration of opteyo";
+
+  inputs = {
+    # Specify the source of Home Manager and Nixpkgs.
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { nixpkgs, home-manager, ... }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in {
+        formatter = pkgs.nixfmt-rfc-style;
+        homeConfigurations."opteyo" = home-manager.lib.homeManagerConfiguration {
+          inherit pkgs;
+
+          # Specify your home configuration modules here, for example,
+          # the path to your home.nix.
+          modules = [ ./home.nix ];
+
+          # Optionally use extraSpecialArgs
+          # to pass through arguments to home.nix
+        };
+      };
+}
--- install.sh
+++ install.sh
@@ -4,37 +4,21 @@ set -ue
 
 pushd $(dirname ${BASH_SOURCE:-$0})
 
-sudo apt-get update -y
-sudo apt-get install -y bison ncurses-dev pkg-config libssl-dev
+# TODO: test: install nix /w nix-installer
+# curl --proto '=https' --tlsv1.2 -sSf -L https://install.determinate.systems/nix | sh -s -- install
+# https://github.com/DeterminateSystems/nix-installer?tab=readme-ov-file#without-systemd-linux-only
+curl --proto '=https' --tlsv1.2 -sSf -L https://install.determinate.systems/nix | sh -s -- install linux \
+  --extra-conf "sandbox = false" \
+  --init none \
+  --no-confirm
 
-function install_zsh() {
-    if ! command -v mise >/dev/null 2>&1; then
-        sudo apt-get update
-        sudo apt-get install -y zsh
-        echo -e "\e[36mInstalled zsh\e[m\n"
-    fi
-}
+# TODO: link config files /w home-manager
 
-function install_mise() {
-    if ! command -v mise >/dev/null 2>&1; then
-        curl https://mise.run | sh
-        echo -e "\e[36mInstalled mise\e[m\n"
-    fi
-}
-
-install_zsh
-# install_yacc
-install_mise
-
-ln -snfv $(pwd)/.config/mise $HOME/.config/mise
 ln -snfv $(pwd)/.config/sheldon $HOME/.config/sheldon
 ln -snfv $(pwd)/.zshrc $HOME/.zshrc
 ln -snfv $(pwd)/.config/zsh $HOME/.config/zsh
 ln -snfv $(pwd)/.ripgreprc $HOME/.ripgreprc
 ln -snfv $(pwd)/.config/systemd $HOME/.config/systemd
-
-mise install
-
 ln -snfv $(pwd)/.gitconfig $HOME/.gitconfig
 
 popd
