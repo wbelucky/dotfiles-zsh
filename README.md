39e0e9e4d25ce1c103934914a093f705cc3407da
wbelucky <39439193+WBelucky@users.noreply.github.com>
2025-04-01 11:06:25 +0900

    add fmt

--- README.md
+++ README.md
@@ -7,3 +7,10 @@ devcontainer up --workspace-folder .
 devcontainer exec --workspace-folder . zsh
 ./install.sh
 ```
+
+nix
+
+```
+sudo su
+nix profile install .#dotfiles-zsh
+```
--- flake.nix
+++ flake.nix
@@ -11,22 +11,26 @@
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { nixpkgs, home-manager, ... }:
+  outputs = { nixpkgs, home-manager, flake-utils, ... }:
     flake-utils.lib.eachDefaultSystem (
       system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
       in {
         formatter = pkgs.nixfmt-rfc-style;
-        homeConfigurations."opteyo" = home-manager.lib.homeManagerConfiguration {
-          inherit pkgs;
+        legacyPackages = {
+          inherit (pkgs) home-manager;
+          homeConfigurations."opteyo" = home-manager.lib.homeManagerConfiguration {
+            inherit pkgs;
 
-          # Specify your home configuration modules here, for example,
-          # the path to your home.nix.
-          modules = [ ./home.nix ];
+            # Specify your home configuration modules here, for example,
+            # the path to your home.nix.
+            modules = [ ./home.nix ];
 
-          # Optionally use extraSpecialArgs
-          # to pass through arguments to home.nix
+            # Optionally use extraSpecialArgs
+            # to pass through arguments to home.nix
+          };
         };
-      };
+      }
+    );
 }
