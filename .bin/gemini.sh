c268202178b93179faeed1ae8cffbc69a4d05e36
wbelucky <39439193+WBelucky@users.noreply.github.com>
2025-05-04 17:29:51 +0900

    add gemini

---
+++ .bin/gemini.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+curl "https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=${GEMINI_API_KEY}" \
+	-X POST \
+	--json "$(jq --arg text "$([ -z "$1"] && cat - || echo "$1")" -n '.contents[0].parts[0].text = $text')" | jq -r ".candidates[0].content.parts[0].text"
