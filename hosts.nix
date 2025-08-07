0850ad2535797bf37f33a41352d5a6d8c67ee732
wbelucky <39439193+WBelucky@users.noreply.github.com>
2025-08-07 15:24:12 +0900

    fix a bug

--- hosts.nix
+++ hosts.nix
@@ -8,7 +8,7 @@
     home.username = "wbelucky";
     home.homeDirectory = "/home/wbelucky";
   };
-  "DESKTOP-8VQ0RA4" = {lib, pkgs, config}@args: {
+  "DESKTOP-8VQ0RA4" = {lib, pkgs, config, ...}@args: {
     # home.packages = config.home.packages ++ (with pkgs; [drawio]);
     home.username = "biraki";
     home.homeDirectory = "/home/biraki";
