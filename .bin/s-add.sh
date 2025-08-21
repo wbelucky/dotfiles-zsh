d6e2611a2f2c5f403ce6b3fb7351485da5ebd802
wbelucky <39439193+WBelucky@users.noreply.github.com>
2025-08-21 18:04:42 +0900

    fix a bug

--- .bin/s-add.sh
+++ .bin/s-add.sh
@@ -2,6 +2,6 @@
 
 session_file="$ZK_NOTEBOOK_DIR/sessions/$(date +%y-%m).yaml"
 note="$1" content="${2}" yq -i '
-.sessions += [{"start": (now | format_datetime("2006-01-02T00:00:00")), "end": (now | format_datetime("2006-01-02T00:00:00")) "note": strenv(note), "content": strenv(content), "id": .last_id + 1}]
+.sessions += [{"start": (now | format_datetime("2006-01-02T00:00:00")), "end": (now | format_datetime("2006-01-02T00:00:00")), "note": strenv(note), "content": strenv(content), "id": .last_id + 1}]
 , .last_id = .last_id + 1' $session_file
 
