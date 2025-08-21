0e65aa48599da1e107676f7f47ed21e035a79652
wbelucky <39439193+WBelucky@users.noreply.github.com>
2025-08-21 15:28:46 +0900

    fix s-start.sh

--- .bin/s-start.sh
+++ .bin/s-start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-note="$1" yq -i '
-.sessions += [{"start": (now | format_datetime("2006-01-02T15:04:05")), "note": strenv(note)}]
+note="$1" content="${2:-''}" yq -i '
+.sessions += [{"start": (now | format_datetime("2006-01-02T15:04:05")), "note": strenv(note), "content": strenv(content)}]
 ' $ZK_NOTEBOOK_DIR/sessions/$(date +%y-%m).yaml
