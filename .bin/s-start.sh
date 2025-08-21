61a9b2951413555333062ecedd85c2d5fc55fd38
wbelucky <39439193+WBelucky@users.noreply.github.com>
2025-08-21 17:37:19 +0900

    fix s-start.sh

--- .bin/s-start.sh
+++ .bin/s-start.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
-note="$1" content="${2:-''}" yq -i '
-.sessions += [{"start": (now | format_datetime("2006-01-02T15:04:05")), "note": strenv(note), "content": strenv(content)}]
-' $ZK_NOTEBOOK_DIR/sessions/$(date +%y-%m).yaml
+session_file="$ZK_NOTEBOOK_DIR/sessions/$(date +%y-%m).yaml"
+if [[ $(yq '.sessions[-1] | has("end") | not' $session_file) = 'true' ]]; then
+	SCRIPT_DIR=$(cd $(dirname $0) && pwd)
+	source "$SCRIPT_DIR/s-end.sh"
+fi
+note="$1" content="${2}" yq -i '
+.sessions += [{"start": (now | format_datetime("2006-01-02T15:04:05")), "note": strenv(note), "content": strenv(content), "id": .last_id + 1}]
+, .last_id = .last_id + 1' $session_file
